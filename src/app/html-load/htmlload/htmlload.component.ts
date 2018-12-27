4e372e4017247a8ce22ed47d7c5b23ee41d6d03e
Sander Elias <sanderelias+git@gmail.com>
2018-12-27 06:31:41 +0100

    added correct host setting

--- src/app/html-load/htmlload/htmlload.component.ts
+++ src/app/html-load/htmlload/htmlload.component.ts
@@ -4,9 +4,16 @@ import { BogusLoadService } from '../bogus-load.service';
 @Component({
   selector: 'app-htmlload',
   templateUrl: './htmlload.component.html',
-  styles: []
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ]
 })
 export class HtmlloadComponent implements OnInit {
+  /** gets the native element.  */
   private elm = this.elmRef.nativeElement as HTMLElement;
 
   constructor(private html: BogusLoadService, private elmRef: ElementRef) {}
@@ -14,7 +21,6 @@ export class HtmlloadComponent implements OnInit {
   ngOnInit() {
     /** I don't need to unsubscribe, because htmlLoad completes */
     this.html.load('Some url provided').subscribe(html => {
-      console.log('html loaded', html, this.elm);
       /**
        * setting innerHTML doesn't "run" scripts,
        * instead create a document fragment
