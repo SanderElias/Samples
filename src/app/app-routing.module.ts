51affb6ee77cc1abeb6a82335051f03f8def301e
Sander Elias <sanderelias+git@gmail.com>
2018-12-27 06:38:52 +0100

    Switched to relative paths ivm stackBlitz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path: 'filterSamp',
-    loadChildren: 'src/app/filter-samp/filter-samp.module#FilterSampModule'
+    loadChildren: '../../src/app/filter-samp/filter-samp.module#FilterSampModule'
   },
-  { path: 'modalSamp', loadChildren: 'src/app/modal-samp/modal-samp.module#ModalSampModule'},
-  { path: 'mixinSamp', loadChildren: 'src/app/mixins/mixins.module#MixinsModule'},
-  {path: 'htmlLoad', loadChildren: 'src/app/html-load/html-load.module#HtmlLoadModule'},
+  { path: 'modalSamp', loadChildren: '../../src/app/modal-samp/modal-samp.module#ModalSampModule'},
+  { path: 'mixinSamp', loadChildren: '../../src/app/mixins/mixins.module#MixinsModule'},
+  {path: 'htmlLoad', loadChildren: '../../src/app/html-load/html-load.module#HtmlLoadModule'},
   { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'home' }
 ];
