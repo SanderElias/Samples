9d42911ae9370c020f029fa399d8f7f1a1c17597
Sander Elias <sanderelias+git@gmail.com>
2018-12-28 07:57:21 +0100

    moved to a hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterContentInit
+} from '@angular/core';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
+  /** the 'input' element that toggles the menu */
+  checked: HTMLInputElement;
+  /** a setter function to hook up the above */
+  @ViewChild('menuToggle') set _toggle(x: ElementRef) {
+    if (x && !this.checked) {
+      this.checked = x.nativeElement as HTMLInputElement;
+    }
+  }
   title = 'samples';
+  constructor(private elmRef: ElementRef) {}
+
+  ngAfterContentInit() {
+    /**
+     * I want the menu to close when a view is selected
+     * Q&D: add click-handler to every link.
+     */
+    const host = this.elmRef.nativeElement as HTMLElement;
+    if (host) {
+      const menuItems = host.querySelectorAll('#menu>a');
+      Array.from(menuItems).forEach(a =>
+        a.addEventListener('click', () => {
+          this.checked.checked = false;
+        })
+      );
+    }
+  }
 }
