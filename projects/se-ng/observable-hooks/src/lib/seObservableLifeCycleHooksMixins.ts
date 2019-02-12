6c4688583a76151c28aeb8213e891165208f3a61
Sander Elias <sanderelias+git@gmail.com>
2019-02-12 06:24:48 +0100

    refactored subjects to be symbols. workaournd for TS issue with privates in mixins.

--- projects/se-ng/observable-hooks/src/lib/seObservableLifeCycleHooksMixins.ts
+++ projects/se-ng/observable-hooks/src/lib/seObservableLifeCycleHooksMixins.ts
@@ -13,6 +13,15 @@ import {
 import { Subject } from 'rxjs';
 export type Constructor<T = {}> = new (...args: any[]) => T;
 
+const _acc = Symbol('_acc');
+const _aci = Symbol('_aci');
+const _avc = Symbol('_avc');
+const _avi = Symbol('_avi');
+const _dc = Symbol('_dc');
+const _oc = Symbol('_oc');
+const _oi = Symbol('_oi');
+const _destroy = Symbol('_destroy');
+
 interface HooksMixin {
   // tslint:disable-next-line:callable-types
   <T extends Constructor>(base: T): T;
@@ -23,28 +32,28 @@ interface HooksMixin {
  */
 export const seOnDestroy$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements OnDestroy {
-    private _destroy = new Subject<void>();
+    [_destroy] = new Subject<void>();
     /** observable that emits once on destroy */
-    seOnDestroy$ = this._destroy.asObservable();
+    seOnDestroy$ = this[_destroy].asObservable();
     ngOnDestroy() {
       super['ngOnDestroy'] && super['ngOnDestroy']();
-      this._destroy.next();
-      this._destroy.complete();
+      this[_destroy].next();
+      this[_destroy].complete();
     }
   };
 
 /** extends component with an observable seAfterContentChecked$ lifecycle hook */
 export const seAfterContentChecked$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements AfterContentChecked, OnDestroy {
-    private _acc = new Subject<void>();
+    [_acc] = new Subject<void>();
     /** Observable that emits when content is checked */
-    seAfterContentChecked$ = this._acc.asObservable();
+    seAfterContentChecked$ = this[_acc].asObservable();
     ngAfterContentChecked(): void {
-      this._acc.next();
+      this[_acc].next();
       super['ngAfterContentChecked'] && super['ngAfterContentChecked']();
     }
     ngOnDestroy() {
-      this._acc.complete();
+      this[_acc].complete();
       super['ngOnDestroy'] && super['ngOnDestroy']();
     }
   };
@@ -52,14 +61,14 @@ export const seAfterContentChecked$ = <T extends Constructor>(base: T = class {}
 /** extends component with an observable seAfterContentInit$ lifecycle hook */
 export const seAfterContentInit$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements AfterContentInit {
-    private _afi = new Subject<void>();
+     [_aci] = new Subject<void>();
     /** `Observable<void>` that emits once and completes afer content init */
-    seAfterContentInit$ = this._afi.asObservable();
+    seAfterContentInit$ = this[_aci].asObservable();
     ngAfterContentInit(): void {
       // take this to the end off the que, so the template has some time to init.
       Promise.resolve().then(() => {
-        this._afi.next();
-        this._afi.complete();
+        this[_aci].next();
+        this[_aci].complete();
       });
       super['ngAfterContentInit'] && super['ngAfterContentInit']();
     }
@@ -68,15 +77,15 @@ export const seAfterContentInit$ = <T extends Constructor>(base: T = class {} as
 /** extends component with an observable seAfterViewChecked$ lifecycle hook */
 export const seAfterViewChecked$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements AfterViewChecked, OnDestroy {
-    private _afc = new Subject<void>();
+     [_avc] = new Subject<void>();
     /** observable that emits after the view is checked */
-    seAfterViewChecked$ = this._afc.asObservable();
+    seAfterViewChecked$ = this[_avc].asObservable();
     ngAfterViewChecked(): void {
-      this._afc.next();
+      this[_avc].next();
       super['ngAfterViewChecked'] && super['ngAfterViewChecked']();
     }
     ngOnDestroy() {
-      this._afc.complete();
+      this[_avc].complete();
       super['ngOnDestroy'] && super['ngOnDestroy']();
     }
   };
@@ -84,14 +93,14 @@ export const seAfterViewChecked$ = <T extends Constructor>(base: T = class {} as
 /** extends component with an observable seAfterViewInit$ lifecycle hook */
 export const seAfterViewInit$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements AfterViewInit {
-    private _avi = new Subject<void>();
+    [_avi] = new Subject<void>();
     /** observable that emits and completes after view init */
-    seAfterViewInit$ = this._avi.asObservable();
+    seAfterViewInit$ = this[_avi].asObservable();
     ngAfterViewInit(): void {
       // take this to the end off the que, so the template has some time to init.
       Promise.resolve().then(() => {
-        this._avi.next();
-        this._avi.complete();
+        this[_avi].next();
+        this[_avi].complete();
       });
       // tslint:disable-next-line:no-unused-expression
       super['ngAfterViewInit'] && super['ngAfterViewInit']();
@@ -101,15 +110,15 @@ export const seAfterViewInit$ = <T extends Constructor>(base: T = class {} as T)
 /** extends component with an observable seDoCheck$ lifecycle hook */
 export const seDoCheck$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements DoCheck, OnDestroy {
-    private _dc = new Subject<void>();
+     [_dc] = new Subject<void>();
     /** observable that hits on every round of change detection */
-    seDoCheck$ = this._dc.asObservable();
+    seDoCheck$ = this[_dc].asObservable();
     ngDoCheck(): void {
-      this._dc.next();
+      this[_dc].next();
       super['ngDoCheck'] && super['ngDoCheck']();
     }
     ngOnDestroy() {
-      this._dc.complete();
+      this[_dc].complete();
       super['ngOnDestroy'] && super['ngOnDestroy']();
     }
   };
@@ -117,15 +126,15 @@ export const seDoCheck$ = <T extends Constructor>(base: T = class {} as T) =>
 /** extends component with an observable seOnChanges$ lifecycle hook */
 export const seOnChanges$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements OnChanges, OnDestroy {
-    private _oc = new Subject<SimpleChanges>();
+     [_oc] = new Subject<SimpleChanges>();
     /** observable that fires on changes with a payload of SimpleChanges */
-    seOnChanges$ = this._oc.asObservable();
+    seOnChanges$ = this[_oc].asObservable();
     ngOnChanges(changes: SimpleChanges): void {
-      this._oc.next(changes);
+      this[_oc].next(changes);
       super['ngOnChanges'] && super['ngOnChanges']();
     }
     ngOnDestroy() {
-      this._oc.complete();
+      this[_oc].complete();
       super['ngOnDestroy'] && super['ngOnDestroy']();
     }
   };
@@ -133,14 +142,14 @@ export const seOnChanges$ = <T extends Constructor>(base: T = class {} as T) =>
 /** extends component with an observable seOnInit$ lifecycle hook */
 export const seOnInit$ = <T extends Constructor>(base: T = class {} as T) =>
   class extends base implements OnInit {
-    private _init = new Subject<void>();
+    [_oi] = new Subject<void>();
     /** `Observable<void>` that emites and completes on init. */
-    seOnInit$ = this._init.asObservable();
+    seOnInit$ = this[_oi].asObservable();
     ngOnInit(): void {
       // take this to the end off the que, so the template has some time to init.
       Promise.resolve().then(() => {
-        this._init.next();
-        this._init.complete();
+        this[_oi].next();
+        this[_oi].complete();
       });
       super['ngOnInit'] && super['ngOnInit']();
     }
