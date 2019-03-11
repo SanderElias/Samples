fb71fad6adfc544881dcd7ae2cd0157918412a66
SanderElias <sanderelias+git@gmail.com>
2019-03-11 14:49:56 +0100

    clean up swapi service

--- projects/se-ng/swapi/src/lib/swapi.service.ts
+++ projects/se-ng/swapi/src/lib/swapi.service.ts
@@ -1,20 +1,8 @@
 // tslint:disable:member-ordering
 // import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { concat, EMPTY, from, Observable, of } from 'rxjs';
-import {
-  concatMap,
-  expand,
-  filter,
-  map,
-  mergeMap,
-  reduce,
-  shareReplay,
-  take,
-  toArray,
-  tap,
-  concatAll
-} from 'rxjs/operators';
+import { concat, EMPTY, from, Observable } from 'rxjs';
+import { concatAll, concatMap, expand, filter, map, mergeMap, reduce, shareReplay, take, tap, toArray } from 'rxjs/operators';
 import { addToCache, cacheHas, getFromCache, initCache } from './cache';
 import { Film, FilmsRoot } from './FilmsRoot.interface';
 import { PeopleRoot, Person } from './PeopleRoot.interface';
@@ -25,9 +13,12 @@ import { PeopleRoot, Person } from './PeopleRoot.interface';
 export class SwapiService {
   baseUrl = `https://swapi.co/api/`;
   swapiRoot: SwapiRoot;
-  // Get a page of people
-  //   next?: any; // url of next page (if there is one)
-  //   results: People;
+  /**
+   * Helper. uses fetch to load data from an URL
+   * first checks the indexedDB cache if the data is already there,
+   * otherwise use fetch (I needed promises here) to fetch live data
+   * returns  promise that will hold the result of the URL
+   */
   private load = async <T>(url: string): Promise<T> => {
     await initCache();
     if (!cacheHas(url)) {
@@ -51,10 +42,7 @@ export class SwapiService {
     map((r: PeopleRoot) => r.results),
 
     // scan to accumulate the pages (emitted by expand)
-    reduce<Person[]>(
-      (allPeople, pageOfPeople) => allPeople.concat(pageOfPeople),
-      []
-    ),
+    reduce<Person[]>((allPeople, pageOfPeople) => allPeople.concat(pageOfPeople), []),
 
     map(persons =>
       persons.map(
@@ -67,6 +55,7 @@ export class SwapiService {
     shareReplay(1)
   );
 
+  /** find a person by ID (the url) (deprecated) */
   findById = (id: number): Observable<Person> =>
     this.swPeople$.pipe(
       map(list => list[id]),
@@ -74,22 +63,25 @@ export class SwapiService {
       take(1)
     );
 
-  swFilms$ = from(this.load<FilmsRoot>(`${this.baseUrl}films/`)).pipe(
-    shareReplay(1)
-  );
+  /** load all the films (deprecated) */
+  swFilms$ = from(this.load<FilmsRoot>(`${this.baseUrl}films/`)).pipe(shareReplay(1));
 
+  /** helper to fetch all the page of an swapi root endpoint */
   getAllPagedData(url): Observable<any> {
     return from(this.load(`${url}`)).pipe(
+      /** use the expand operator to feed in the remaining pages */
       expand(r => (r['next'] ? this.load(r['next']) : EMPTY))
     );
   }
 
+  /** find a film by url (deprecated) */
   findFilmByUrl = (url: string): Observable<Film> =>
     this.swFilms$.pipe(
       map(films => films.results.find(film => film.url === url)),
       take(1)
     );
 
+  /** helper to fetch Count random person(s), that has also some loaded films in there */
   getRandomPerson = (count = 1): Observable<Person> =>
     from(Array.from({ length: count })).pipe(
       concatMap(() =>
@@ -109,7 +101,8 @@ export class SwapiService {
       )
     );
 
-  constructor(/*private http: HttpClient*/) {
+  constructor() {
+    /** kick of loading the data */
     this.loadData();
   }
 
@@ -118,46 +111,45 @@ export class SwapiService {
    * @param url
    */
   get<T>(url: string): Observable<T> {
-    const base = Object.values(this.swapiRoot).find(top =>
-      url.toLowerCase().includes(url.toLowerCase())
-    );
+    const base = Object.values(this.swapiRoot).find(top => url.toLowerCase().includes(url.toLowerCase()));
 
     if (base) {
-      return this.getAllPagedData(base).pipe(
-        map((baseData: any) => baseData.results.find(row => row.url === url))
-      );
+      return this.getAllPagedData(base).pipe(map((baseData: any) => baseData.results.find(row => row.url === url)));
     }
     return from(this.load(url) as Promise<T>);
   }
 
   /** load everything from the SWAPI.co into indexedDB */
   loadData() {
-    from(this.load(this.baseUrl))
+    /** start off with the 'root' of swapu */
+    from(this.load<SwapiRoot>(this.baseUrl))
       .pipe(
-        tap(swapiRoot => (this.swapiRoot = this.swapiRoot)),
+        /** use a side-effect to store it in this service */
+        tap(swapiRoot => (this.swapiRoot = swapiRoot)),
+        /** loop over all listed endpoints */
         concatMap(r => Object.values(r).map(url => this.getAllPagedData(url))),
+        /** combine all observbles form above into a results stream */
         concatAll(),
-        toArray(),
+        /** change that into an array (Only needed when going to display) */
+        toArray()
         /** log result */
-        tap(r => console.log(r))
+        // tap(r => console.log(r))
       )
       .subscribe();
   }
 
   findWithName = (name: string) =>
-    this.swPeople$.pipe(
-      map(list =>
-        list.find(row =>
-          row.name.toLowerCase().includes(name.toLowerCase().trim())
-        )
-      )
-    );
+    this.swPeople$.pipe(map(list => list.find(row => row.name.toLowerCase().includes(name.toLowerCase().trim()))));
 }
 
 function getRandomDateInPast() {
+  /** a year between 1000...2000 */
   const year = Math.ceil(Math.random() * 1000) + 1000;
+  /** pick a random month */
   const month = Math.floor(Math.random() * 12);
+  /** pic a random save date, (27 bcs Februari) */
   const day = Math.ceil(Math.random() * 27);
+  /** combine into a date, use noon bcs time-zone's */
   return new Date(year, month, day, 12, 0);
 }
 
