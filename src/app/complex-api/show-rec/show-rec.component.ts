1fbaf1f995013d3e2ede9751f91473f7df53662e
SanderElias <sanderelias+git@gmail.com>
2019-03-14 07:01:30 +0100

    fixed to deep recursion and moved subject into detals

--- src/app/complex-api/show-rec/show-rec.component.ts
+++ src/app/complex-api/show-rec/show-rec.component.ts
@@ -23,9 +23,17 @@ export class ShowRecComponent implements OnInit {
 
   key = (e: Entry) => e[0];
   val = (e: Entry) => e[1];
-  isRec = (e: Entry) => Array.isArray(e[1]);
 
   constructor() {}
+
+  isRecArray(e:Entry) {
+    const i = e[1];
+    if (Array.isArray(i) && i.length>0) {
+      const firstEnt = i[0];
+      return (!Array.isArray(firstEnt)) && typeof firstEnt === 'object'
+    }
+    return false
+  }
 
   ngOnInit() {}
 }
