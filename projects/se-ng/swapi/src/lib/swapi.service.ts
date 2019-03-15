10bb030edcbd9ef50ffc3c0462fde87f68567c19
Sander Elias <sanderelias+git@gmail.com>
2019-03-15 05:06:51 +0100

    make better use of cache

--- projects/se-ng/swapi/src/lib/swapi.service.ts
+++ projects/se-ng/swapi/src/lib/swapi.service.ts
@@ -41,9 +41,10 @@ export class SwapiService {
         .catch(e => undefined);
       await addToCache(url, liveData);
     }
-
     return (getFromCache(url) as unknown) as T;
   };
+
+  /** the 'root' of swapi, as an reuable observable. */
   swapiRoot$ = from(this.load<SwapiRoot>(this.baseUrl)).pipe(
     tap(r => (this.swapiRoot = r)),
     shareReplay(1)
@@ -60,7 +61,10 @@ export class SwapiService {
     map((r: PeopleRoot) => r.results),
 
     // scan to accumulate the pages (emitted by expand)
-    reduce<Person[]>((allPeople, pageOfPeople) => allPeople.concat(pageOfPeople), []),
+    reduce<Person[]>(
+      (allPeople, pageOfPeople) => allPeople.concat(pageOfPeople),
+      []
+    ),
 
     map(persons =>
       persons.map(
@@ -82,7 +86,9 @@ export class SwapiService {
     );
 
   /** load all the films (deprecated) */
-  swFilms$ = from(this.load<FilmsRoot>(`${this.baseUrl}films/`)).pipe(shareReplay(1));
+  swFilms$ = from(this.load<FilmsRoot>(`${this.baseUrl}films/`)).pipe(
+    shareReplay(1)
+  );
 
   /** helper to fetch all the page of an swapi root endpoint */
   getAllPagedData(url): Observable<any> {
@@ -120,8 +126,8 @@ export class SwapiService {
     );
 
   constructor() {
-    /** kick of loading the data */
-    this.loadData();
+    /** kick of loading the data, and keep it alive during entire app */
+    this.loadData().subscribe();
   }
 
   /**
@@ -129,10 +135,14 @@ export class SwapiService {
    * @param url
    */
   get<T>(url: string): Observable<T> {
-    const base = Object.values(this.swapiRoot).find(top => url.toLowerCase().includes(url.toLowerCase()));
+    const base = Object.values(this.swapiRoot).find(top =>
+      url.toLowerCase().includes(url.toLowerCase())
+    );
 
     if (base) {
-      return this.getAllPagedData(base).pipe(map((baseData: any) => baseData.results.find(row => row.url === url)));
+      return this.getAllPagedData(base).pipe(
+        map((baseData: any) => baseData.results.find(row => row.url === url))
+      );
     }
     return from(this.load(url) as Promise<T>);
   }
@@ -140,49 +150,60 @@ export class SwapiService {
   /** load everything from the SWAPI.co into indexedDB */
   loadData() {
     /** start off with the 'root' of swapi */
-    this.swapiRoot$
-      .pipe(
-        /** use a side-effect to store it in this service */
-        tap(swapiRoot => (this.swapiRoot = swapiRoot)),
-        /** loop over all listed endpoints */
-        concatMap(r => Object.values(r).map(url => this.getAllPagedData(url))),
-        /** combine all observables form above into a results stream */
-        concatAll(),
-        /** change that into an array (Only needed when going to display) */
-        toArray()
-        /** log result */
-        // tap(r => console.log('loadedInApi',r))
-      )
-      .subscribe();
+    return this.swapiRoot$.pipe(
+      /** use a side-effect to store it in this service */
+      tap(swapiRoot => (this.swapiRoot = swapiRoot)),
+      /** loop over all listed endpoints */
+      concatMap(r => Object.values(r).map(url => this.getAllPagedData(url))),
+      /** combine all observables form above into a results stream */
+      concatAll(),
+      /** change that into an array (Only needed when going to display) */
+      toArray()
+      /** log result */
+      // tap(r => console.log('loadedInApi',r))
+    );
   }
 
   enrich<T>(rec: T): Observable<T> {
+    /**
+     * Helper to create an observable<{propname:value}> for each property
+     */
+    const keyHandler = propName => {
+      const value = rec[propName];
+      if (Array.isArray(value) && value.length > 0) {
+        /** make an observable from the array in the current prop */
+        return from<string[]>(rec[propName]).pipe(
+          /** turn each url to an observable */
+          concatMap(url => this.findByUrl(url)),
+          /** combine the array of observables to an array */
+          toArray(),
+          /** make it into an object that has the props name, and the result */
+          map(arr => ({ [propName]: arr }))
+        );
+      }
+      if (typeof value === 'string') {
+        /** is it an url from a known set? */
+        const subSet = this.detectSet(value);
+        /** yes? load it from the set, otherwise just return the value */
+        return of(subSet ? this.findByUrl(value) : value).pipe(
+          map(x => ({ [propName]: x }))
+        );
+      }
+      return of(value).pipe(map(x => ({ [propName]: x })));
+    };
+
     return this.swapiRoot$.pipe(
-      concatMap(root =>
-        /** make an array of observables from eligible keys */
-        Object.keys(rec)
-          /** check for key's that are in root */
-          .filter(prop => Object.keys(root).includes(prop))
-          /** traverse every array in each key */
-          .map(key =>
-            /** make an observable from the array in the current prop */
-            from<string[]>(rec[key]).pipe(
-              /** turn each url to an observable */
-              concatMap(url => this.load(url)),
-              /** combine the array of observables to an array */
-              toArray(),
-              /** make it into an object that has the props name, and the result */
-              map(arr => ({ [key]: arr }))
-            )
-          )
-      ),
+      /** Split out the record to an array of observables, one for each property */
+      concatMap(root => Object.keys(rec).map(keyHandler)),
       /** flatten the array of observables into results */
       concatAll(),
-      /** reduce all the results into 1 object */
-      reduce((combine, res) => ({ ...combine, ...res }), {}),
-      /** merge the outcome with the original record */
-      map(combined => ({ ...rec, ...combined })),
-      catchError((e: Error) => of(({ message: e.message } as unknown) as T))
+      /** reduce all the results back into 1 object */
+      reduce((combine, res) => ({ ...combine, ...res }), {} as T),
+      /** complete but don't emit a result if there is an error */
+      catchError((e: Error) => {
+        console.warn(e);
+        return of({} as T);
+      })
     );
   }
 
@@ -209,9 +230,39 @@ export class SwapiService {
   findIn = (selectedSet: keyof SwapiRoot, nameOrTitle: string) =>
     this.getAllRows(selectedSet).pipe(
       map(list =>
-        list.find(row => (row.name || row.title || '').toLowerCase().includes(nameOrTitle.toLowerCase().trim()))
+        list.find(row =>
+          (row.name || row.title || '')
+            .toLowerCase()
+            .includes(nameOrTitle.toLowerCase().trim())
+        )
       )
     );
+
+  /**
+   * Helper to detect the table of an url. return the name of the set, or undefined/false
+   * @param url String to test
+   */
+  detectSet(url) {
+    if (typeof url === 'string') {
+      const prop = Object.entries(this.swapiRoot).find(
+        ([setName, setBaseUrl]) => url.includes(setBaseUrl)
+      );
+      return (prop && ((prop[0] as unknown) as keyof SwapiRoot)) || undefined;
+    }
+  }
+
+  /**
+   * Helper function to search by url
+   * takes the name of the set, and the url
+   */
+  findByUrl = (url: string) => {
+    const selectedSet = this.detectSet(url);
+    if (selectedSet) {
+      return this.getAllRows(selectedSet).pipe(
+        map(list => list.find(row => row.url.trim() === url.trim()))
+      );
+    }
+  };
 
   /**
    * find people by name (deprecated)
