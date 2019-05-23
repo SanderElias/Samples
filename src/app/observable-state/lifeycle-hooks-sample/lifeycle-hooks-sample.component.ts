2c86ef12cf8f7d4734175f2693ec3535fddd9ce9
SanderElias <sanderelias+git@gmail.com>
2019-05-23 12:41:37 +0200

    fixed impor

--- src/app/observable-state/lifeycle-hooks-sample/lifeycle-hooks-sample.component.ts
+++ src/app/observable-state/lifeycle-hooks-sample/lifeycle-hooks-sample.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SwapiService, SpaceShip} from '@se-ng/swapi';
-import {seOnInit, seOnDestroy} from '@se-ng/observable-hooks/bundles/se-ng-observable-hooks.umd';
+import {seOnInit, seOnDestroy} from '@se-ng/observable-hooks';
 import {switchMap, shareReplay, map, startWith, tap} from 'rxjs/operators';
 import {modelFromLatest} from '../../../../src/utils/modelFromLatest';
 import {FormControl} from '@angular/forms';
