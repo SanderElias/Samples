feb4a6cf4c3b2205285d9a6a8b0b49acbc482578
Sander Elias <sanderelias+git@gmail.com>
2019-06-01 07:04:53 +0200

    fixed orphin components

--- src/app/rijks/rijks/rijks.module.ts
+++ src/app/rijks/rijks/rijks.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtDetailComponent } from '../art-detail/art-detail.component';
 import { ArtistsComponent } from '../artists/artists.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ArtDetailComponent, ArtistsComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class RijksModule {}
