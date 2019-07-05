41e2f33aa6194211aca255272c65fbea58538b7a
SanderElias <sanderelias+git@gmail.com>
2019-07-05 05:41:41 +0200

    migration to 8 fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ArtDetailComponent } from './rijks/art-detail/art-detail.component';
+import { ArtistsComponent } from './rijks/artists/artists.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ArtDetailComponent
+    ArtDetailComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule,
