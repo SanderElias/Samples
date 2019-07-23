a266e046d1f2b6b5842e177489a592c0c1f7cbe9
SanderElias <sanderelias+git@gmail.com>
2019-07-23 07:46:13 +0200

    added svg calendar module

---
+++ src/app/svg-calendar/svg-calendar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SvgCalenderComponent } from './svg-calender/svg-calender.component';
+
+
+
+@NgModule({
+  declarations: [SvgCalenderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SvgCalendarModule { }
---
+++ src/app/svg-calendar/svg-calender/svg-calender.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-calender',
+  templateUrl: './svg-calender.component.html',
+  styles: []
+})
+export class SvgCalenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
