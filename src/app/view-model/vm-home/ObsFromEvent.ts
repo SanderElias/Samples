f73808ab96a0deb0bde5ec02aac3e22ef2f7988f
SanderElias <sanderelias+git@gmail.com>
2019-08-17 10:03:21 +0200

    reworking viewModel sample

---
+++ src/app/view-model/vm-home/ObsFromEvent.ts
@@ -0,0 +1,41 @@
+import { ElementRef, QueryList } from '@angular/core';
+import { Subject } from 'rxjs';
+
+const EventStream = Symbol('ObsFromEvent');
+
+export function ObsFromEvent<K extends keyof HTMLElementEventMap>(
+  eventName: K
+): any {
+  return (target: any, propertyKey: string | symbol) => {
+    return {
+      get() {
+        return fetchSubject(this, propertyKey).asObservable();
+      },
+      set(ql: QueryList<ElementRef>) {
+        if (!(ql instanceof QueryList)) {
+          throw new Error('ObsFromEvent expects a QueryList');
+        }
+        const sub = fetchSubject(this, propertyKey);
+        ql.forEach(ref => {
+          if (!(ref instanceof ElementRef)) {
+            throw new Error('ObsFromEvent expects a QueryList<ElementRef>');
+          }
+          const el = ref.nativeElement as HTMLElement;
+          if (!el[EventStream]) {
+            // Don't hook up more as once for each element
+            el.addEventListener(eventName, e => sub.next(e));
+            el[EventStream] = EventStream;
+          }
+        });
+      },
+    };
+  };
+}
+
+function fetchSubject(instance, propertyKey): Subject<Event> {
+  const ref = (instance[EventStream] = instance[EventStream] || {});
+  if (!ref[propertyKey]) {
+    ref[propertyKey] = new Subject<Event>();
+  }
+  return ref[propertyKey];
+}
