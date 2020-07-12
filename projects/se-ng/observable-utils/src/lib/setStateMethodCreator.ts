39b6f82258370d0fe6ac44374466e1bb12afe729
sanderelias <sanderelias+git@gmail.com>
2020-07-12 08:57:55 +0200

    fix v9 issues

--- projects/se-ng/observable-utils/src/lib/setStateMethodCreator.ts
+++ projects/se-ng/observable-utils/src/lib/setStateMethodCreator.ts
@@ -1,27 +1,35 @@
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
+import { BehaviorSubject, ReplaySubject } from "rxjs";
+import { map, take, tap } from "rxjs/operators";
 
 /**
  * Function that takes a state subject and returns a method for your component
  * that allows you to set an individual property on the state observable
  */
-export function createSetStateMethod<T>(stateSubject: ReplaySubject<T> | BehaviorSubject<T>) {
+export function createSetStateMethod<T>(
+  stateSubject: ReplaySubject<T> | BehaviorSubject<T>
+) {
   /**
    * A state helper method that allows you to easily update the state with
    */
   function setState(newState: Partial<T>): Promise<T>;
   function setState<K extends keyof T>(property: K, newValue: T[K]): Promise<T>;
-  function setState<K extends keyof T>(prop: K | Partial<T>, newValue?: T[K]): Promise<T> {
+  function setState<K extends keyof T>(
+    prop: K | Partial<T>,
+    newValue?: T[K]
+  ): Promise<T> {
     return stateSubject
       .pipe(
         take(1), // Only update once!
-        map(currentState => {
-          if (typeof prop === 'string') {
+        map((currentState) => {
+          if (typeof prop === "string") {
             return { ...currentState, [prop]: newValue };
           }
-          return { ...currentState, ...prop };
+          if (typeof prop === "object") {
+            return { ...currentState, ...prop };
+          }
+          throw new Error("Property needs to be a string or an object");
         }), // insert the state.
-        tap(newState => stateSubject.next(newState)) // push the update to the view.
+        tap((newState) => stateSubject.next(newState)) // push the update to the view.
       )
       .toPromise(); // return a promise with handled state, normally unused.
   }
