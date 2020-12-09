2181ebcd59c5f9143d501ffeecf841a837faa3c5
sanderelias <sanderelias+git@gmail.com>
2020-12-09 07:37:28 +0100

    update to v11

--- projects/slipnslides/src/app/edit/select-file/select-file.component.spec.ts
+++ projects/slipnslides/src/app/edit/select-file/select-file.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SelectFileComponent } from './select-file.component';
 
@@ -6,7 +6,7 @@ describe('SelectFileComponent', () => {
   let component: SelectFileComponent;
   let fixture: ComponentFixture<SelectFileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectFileComponent ]
     })
