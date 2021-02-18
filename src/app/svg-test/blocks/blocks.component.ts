c2ad623a7c4ac2d51f63f6e9718511b9b63619ad
sanderelias <sanderelias+git@gmail.com>
2021-02-18 07:08:59 +0100

    Added a smith of comments

--- src/app/svg-test/blocks/blocks.component.ts
+++ src/app/svg-test/blocks/blocks.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
-import { timer } from 'rxjs';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-blocks',
@@ -15,20 +14,24 @@ import { timer } from 'rxjs';
 })
 export class BlocksComponent {
   colCount = Math.floor(1000 / 6);
+  /** generate the blocks.. create the ammount that will fit */
   blocks = Array.from({ length: this.colCount * this.colCount }, (_, i) => ({
     fillColor: this.randomColor(),
     x: this.calcX(i),
     y: this.calcY(i),
     id: i,
   }));
+  /** input so e can do something */
   @Input() set recolor(x) {
     if (x) {
       this._recolor();
     }
   }
+  /** fire an event when an rectangle is clicked. */
   @Output() rectClicked = new EventEmitter<number>();
 
-  changeColor(ev: MouseEvent) {
+  /** lick handler */
+  sendRectNumber(ev: MouseEvent) {
     ev.preventDefault();
     ev.stopPropagation();
     const rect = ev.target as SVGRectElement;
@@ -38,19 +41,24 @@ export class BlocksComponent {
     }
   }
 
+  /** calculate column number */
   calcX(i) {
     const rowsDone = Math.floor(i / this.colCount) * this.colCount;
     return (i - rowsDone) * 6;
   }
+
+  /** calculate row number */
   calcY(i) {
     return Math.floor(i / this.colCount) * 6;
   }
 
+  /** give all the blocks a new color */
   _recolor() {
     this.blocks.forEach(block => (block.fillColor = this.randomColor()));
     console.log(`Changed the color of ${this.blocks.length} blocks`);
   }
 
+  /** helper to generate a random color */
   randomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
--- src/app/svg-test/svg-test.component.ts
+++ src/app/svg-test/svg-test.component.ts
@@ -6,16 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class SvgTestComponent implements OnInit {
+  /** simple counter, for no good reason ;) */
   count = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  /** up the counter */
   reColor() {
     this.count += 1;
   }
 
+  /** log out to console when a block is clicked. */
   bit(n) {
     console.log(`rectangle number ${n} is clicked`)
   }
