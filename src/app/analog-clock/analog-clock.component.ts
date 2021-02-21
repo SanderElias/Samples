801f6e88961e8c93507af9ba61ced831f3595a6b
sanderelias <sanderelias+git@gmail.com>
2021-02-21 08:36:42 +0100

    commeted

--- src/app/analog-clock/analog-clock.component.ts
+++ src/app/analog-clock/analog-clock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-analog-clock',
@@ -6,14 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class AnalogClockComponent implements OnInit {
-  constructor() {}
+  constructor(private zone: NgZone) {}
 
   ngOnInit(): void {
-    analogClock();
+    this.zone.runOutsideAngular(() => {
+      analogClock(getCanvasCtx());
+      function getCanvasCtx() {
+        let canvas = document.querySelector('.canvas') as HTMLCanvasElement;
+        return canvas.getContext('2d');
+      }
+    });
   }
 }
 
-function analogClock() {
+function analogClock(ctx) {
   //  Functional helpers, curry and compose
   function curry(fn, ...args) {
     let _curry = args =>
@@ -24,7 +30,7 @@ function analogClock() {
   const map = curry((fn, value) => value.map(fn));
   const reduce = curry((fn, value) => value.reduce(fn));
 
-  const uncurry = fn => (...args) => args.reduce((ret, cur) => ret(cur), fn);
+  const unCurry = fn => (...args) => args.reduce((ret, cur) => ret(cur), fn);
   const unary = fn => arg => fn(arg);
   const identity = x => x;
   const constant = x => () => x;
@@ -34,7 +40,7 @@ function analogClock() {
     return a + b + c;
   }
   const cSum = curry(sum);
-  console.log(uncurry(cSum)(1, 2, 3));
+  console.log(unCurry(cSum)(1, 2, 3));
   // ----- end of functional helpers
 
   // some helpers to get a nice color pallette! Generator to fetch the next color
@@ -78,7 +84,7 @@ function analogClock() {
   let color = nextColor();
   // ------ end of color helpers
 
-  const ctx = getCanvasCtx();
+  // const ctx = getCanvasCtx();
 
   // use curry and compose to set up each part of the clock
 
@@ -129,6 +135,7 @@ function analogClock() {
     // are outside the wand's reach
     let drawTack = clockLine(153);
     for (let x = 1; x < 61; x += 1) {
+      /** every 5 minuter, do a bigger tack, and write the hour text */
       if (x % 5 === 0) {
         len = 15;
         putText(toDirfrom60(x), '' + x / 5);
@@ -141,12 +148,21 @@ function analogClock() {
     }
   }
 
+  /**
+   * create a function that takes
+   * @param ctx  the canvas
+   * @param origin the 'center' point
+   * @param offset how far of the center will we start drawing
+   * @param length how long s the line we will draw?
+   * @param direction the direction we want to draw into. [x,y], where x and y are below zero indicators
+   */
   function xxLine(ctx, origin, offset, length, direction) {
     let start = [origin[0] + direction[0] * offset, origin[1] + direction[1] * offset];
     let end = [
       origin[0] + direction[0] * (offset + length),
       origin[1] + direction[1] * (offset + length),
     ];
+    /** draw the line */
     line(ctx, start, end);
   }
 
@@ -156,18 +172,27 @@ function analogClock() {
     ctx.font = 'bold 18px Times';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillStyle = '#fff'
+    ctx.fillStyle = '#fff';
     ctx.fillText(text, x, y);
   }
 
+  /** helper creates a function that divides up a circle into P equal parts. */
   function toAngleFrom(p) {
+    /** return function tahat takes a part, and calculates the angle for it. */
     return function (sec) {
-      // 60 seconds/mins === 270 degrees, need some recalulation!
+      // calculate a value to an angle., subtract 90, because we want to start at the top, not at the left.
       let res = sec * (360 / p) - 90;
+      /** because we might end below zero, and the drawing functions don't like that, add a full circle(360) */
       return res < 0 ? res + 360 : res;
     };
   }
 
+  /**
+   * ok, we have the angle, but we need to draw lines to points..
+   * take the angle, and calc the x and y directions we need.
+   * @param angle
+   * @returns
+   */
   function toDirection(angle) {
     let radian = (Math.PI / 180) * angle;
     return [Math.cos(radian), Math.sin(radian)];
@@ -182,11 +207,6 @@ function analogClock() {
 
     // Restore the transform
     context.restore();
-  }
-
-  function getCanvasCtx() {
-    let canvas = document.querySelector('.canvas') as HTMLCanvasElement;
-    return canvas.getContext('2d');
   }
 
   function point(ctx, x, y) {
@@ -204,5 +224,4 @@ function analogClock() {
   }
 
   clock(new Date()); //start of the clock
-
 }
