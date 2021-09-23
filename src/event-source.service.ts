30d3384de7bc83c2b91fc75c8eaf703a4de93a44
sanderelias <sanderelias+git@gmail.com>
2021-09-23 08:19:05 +0200

    Added inMemDb and event generator

---
+++ src/event-source.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDataService } from './in-memory.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventSourceService {
+  faker = import('faker');
+
+  constructor(public db: InMemoryDataService) { }
+
+  async createEvent(maxRows = 25) {
+    const faker = await this.faker;
+    const curDb = this.db.list('contact')
+    const actions = ['update'];
+    if (curDb.length < maxRows) {
+      actions.push('create');
+    }
+    const action = curDb.length < 5 ? 'create' : getWeightedAction();
+    const id = action === 'create' ? '' : curDb[Math.floor(Math.random() * curDb.length)].id;
+    if (action === 'delete') {
+      this.db.delete(id);
+      return { action, id };
+    }
+    const fieldOptions = {
+      name: faker.name.firstName,
+      email: faker.internet.email,
+      // description: faker.lorem.sentence,
+      dob: faker.date.past,
+      street: faker.address.streetAddress,
+      zip: faker.address.zipCode,
+      city: faker.address.city,
+      // image: faker.image.avatar,
+      tag: faker.lorem.word,
+    };
+    const field = Object.keys(fieldOptions)[Math.floor(Math.random() * Object.keys(fieldOptions).length)];
+    const value = fieldOptions[field]();
+    const row = { id, type: 'contact', [field]: value }
+    if (action === 'create') {
+      row.name = faker.name.firstName();
+    }
+    // console.log(row);
+    this.db[action](row);
+    return { action, row };
+  }
+}
+
+const actions = [
+  ['create', 5],
+  ['update', 1000],
+  ['delete', 1]
+].reduce((acc, [action, weight]) => {
+  for (let i = 0; i < weight; i++) {
+    acc.push(action);
+  }
+  return acc;
+}, []);
+
+function getWeightedAction() {
+  return actions[Math.floor(Math.random() * actions.length)];
+}
+
+(async () => {
+  const es = new EventSourceService(new InMemoryDataService());
+  let count = 150000;
+  while (count--) {
+    const r = await es.createEvent();
+  }
+  console.table(es.db.list('contact'));
+})();
---
+++ src/in-memory.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+const randomChar = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+const createId = () => Date.now().toString(36) + '-' + randomChar() + randomChar() + randomChar();
+
+
+export type RowType = 'contact' | 'product' | 'order';
+type Id = string
+export interface Row {
+  id: Id;
+  type: RowType;
+  [field: string]: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+  private inMemDb = new Map<Id, Row>();
+
+  constructor() { }
+  create(row: Row) {
+    const id = row.id || createId();
+    if (typeof id !== 'string') {
+      throw new Error('id must be a string');
+    }
+    if (this.inMemDb.has(row.id)) {
+      throw new Error(`Duplicate id: ${row.id}`);
+    }
+    this.inMemDb.set(id, { ...row, id });
+  }
+  read(id: Id) {
+    return this.inMemDb.get(id);
+  }
+  update(row: Row) {
+    const current = this.inMemDb.get(row.id);
+    if (!current) {
+      throw new Error(`No row with id: ${row.id}`);
+    }
+    this.inMemDb.set(row.id, {...current, ...row});
+  }
+  delete(id: Id) {
+    this.inMemDb.delete(id);
+  }
+  list(type: RowType) {
+    return Array.from(this.inMemDb.values());//.filter(row => row.type === type);
+  }
+}
