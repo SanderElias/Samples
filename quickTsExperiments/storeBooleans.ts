69297de979d1e2517bc3c883be4311187bc9e102
sanderelias <sanderelias+git@gmail.com>
2021-11-06 15:47:37 +0100

    about to upgrade

---
+++ quickTsExperiments/storeBooleans.ts
@@ -0,0 +1,35 @@
+import { setTestabilityGetter } from '@angular/core'
+import { Arr } from 'messagepack'
+
+const gridSize = 4
+const numberOfBits = 5
+const numberOfBytes = Math.ceil((gridSize * gridSize * numberOfBits) / 8)
+const store = new Uint8Array(new ArrayBuffer(numberOfBytes+4))//?
+
+const setSubBit = (x: number, b: boolean, byte: number) => {
+  const mask = 1 << x;
+  return b ? byte |= mask : byte &= ~mask
+}
+
+const setBit = (pos: number, b: boolean) => {
+  const group = Math.floor(pos / 8)
+  const bitpos = pos % 8
+  store[group] = setSubBit(bitpos, true, store[group])
+  // store.set([tmp],group)
+
+}
+
+// setBit(2, true)
+
+
+const showByte = (n: number) => (n + 256)
+  .toString(2).slice(1).split('')
+  .reduce((s, b, i) => s += b + (i % 2 ? '' : ''), '')
+
+const results = [];
+for (let n = 0; n < 100; n += 1) {
+  setBit(n, true)
+  results.push(store.map(n =>n))
+}
+
+console.table(results)
