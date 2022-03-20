6f3ee5b19798b6d3bf7aed405f5e0d8ce7c93988
sanderelias <sanderelias+git@gmail.com>
2022-03-20 11:32:47 +0100

    cloudflare test

---
+++ quickTsExperiments/keyofexp.ts
@@ -0,0 +1,23 @@
+
+
+const someSample:SomeSample = {
+  a: 1 ,
+  hello:"hello",
+  b: new Date(),
+}
+
+interface SomeSample {
+  a: 0|1|boolean;
+  hello:string;
+  b: Date;
+}
+
+
+
+Object.keys(someSample).forEach((key) => {
+  if (key==='a') {
+    someSample[key] = someSample[key] === 1 ? true : false;
+  }
+})
+
+console.dir(someSample);
