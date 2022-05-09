c72cb84eb7411b5220cc7c09faa2e34cf6b45b66
sanderelias <sanderelias+git@gmail.com>
2022-05-09 08:20:10 +0200

    add life-time cache operator to memoize sample

--- quickTsExperiments/memoizeOperator.ts
+++ quickTsExperiments/memoizeOperator.ts
@@ -1,12 +1,12 @@
-import { from, mergeMap, Observable, of, OperatorFunction, pipe, shareReplay, switchMap, tap } from 'rxjs';
+import { from, Observable, of, OperatorFunction, pipe, shareReplay, switchMap, tap } from 'rxjs';
 
 /**
- * 
+ *
  * @param prop the property to use as memoize key
  * @param getByProp a function that will fetch the data if not cached (returns an observable)
  * @returns Operator that memoizes the result of getByProp
  */
-function MemoizeByProperty<T, K extends keyof T>(
+export function memoizeByProperty<T, K extends keyof T>(
   prop: K,
   getByProp: (p: T[K]) => Observable<T>
 ): OperatorFunction<T[K], T> {
@@ -23,6 +23,58 @@ function MemoizeByProperty<T, K extends keyof T>(
   );
 }
 
+export const clearCache = Symbol('clearCache');
+const cacheAll = new Map<string, Map<unknown, unknown>>()
+
+/**
+ * Use an app-lifecycle cache to prevent unnecessary calls to the backend
+ * @param cacheKey string the key to use to cache the result
+ * @param prop K the property to use as memoize key
+ * @param getByProp  (p:T[K]) => Observable<T> a function that will fetch the data if not cached (returns an observable)
+ */
+export function cacheByProperty<T, K extends keyof T>(cacheKey: string, prop: K, getByProp: (p: T[K]) => Observable<T>): OperatorFunction<T[K], T>
+/**
+ * Clears the full cache, unless a cacheKey is provided
+ * @param clearCache symbol the clearCache symbol, used to clear the cache
+ * @param cacheKey string, optional, if provided only the cache for this key will be cleared
+ */
+export function cacheByProperty(clearCache: symbol, cacheKey?: string): void
+export function cacheByProperty<T, K extends keyof T>(...args:
+  [string, K, (p: T[K]) => Observable<T>] |
+  [symbol, string?]
+) {
+  const [cacheKey, ...rest] = args;
+  if (typeof cacheKey === 'symbol') {
+    const [cacheId] = rest as [string];
+    if (cacheId !== undefined) {
+      cacheAll.clear();
+    } else {
+      cacheAll.delete(cacheId);
+    }
+    return;
+  }
+  const [prop, getByProp] = rest as [K, (p: T[K]) => Observable<T>];
+
+  if (!cacheAll.has(cacheKey)) {
+    cacheAll.set(cacheKey, new Map());
+  }
+  const cache = cacheAll.get(cacheKey) as Map<T[K], Observable<T>>;
+  return pipe(
+    switchMap((value: T[K]) => {
+      if (!cache.has(value)) {
+        cache.set(value, getByProp(value).pipe(
+          shareReplay({ bufferSize: 1, refCount: true })
+        ));
+      }
+      return cache.get(value);
+    })
+  );
+}
+
+/**
+ *  demo code after here
+ */
+
 const personGet = (id: number) => {
   /** fake it till you make it. just create a simple object we can use to demo */
   return of({ id, name: `name ${id}` }).pipe(
@@ -31,10 +83,30 @@ const personGet = (id: number) => {
   );
 }
 
-/** create a stream with repeated "keys" do demo */
-from([1, 2, 3, 1, 2, 3, 1, 2, 3]).pipe(
+/**
+ * the "cache" version will only log the create once for both runs
+ * the "memoize" version will log the create for each run
+ */
+from([1, 2, 3, 1, 2, 3]).pipe(
   /** note how your editor will check if the prop is part of the resulting objects. */
-  MemoizeByProperty('id', personGet),
+  cacheByProperty("persons", 'id', personGet),
   tap(p => console.log(p.name)),
+).subscribe()
+
+from([1, 2, 3, 1, 2, 3]).pipe(
+  cacheByProperty("persons", 'id', personGet),
+  tap(p => console.log(p.name)),
+).subscribe()
+
+cacheByProperty(clearCache)
+
+from([1, 2, 3, 1, 2, 3]).pipe(
+  memoizeByProperty('id', personGet),
+  tap(p => console.log('memo', p.name)),
+).subscribe()
+
+from([1, 2, 3, 1, 2, 3]).pipe(
+  memoizeByProperty('id', personGet),
+  tap(p => console.log("memo", p.name)),
 ).subscribe()
 
