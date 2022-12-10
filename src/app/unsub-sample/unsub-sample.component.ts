5d39e5a5d4d221acd214e377722fcd19ba0a4719
sanderelias <sanderelias+git@gmail.com>
2022-12-10 08:56:12 +0100

    finnished unsub sample

--- src/app/unsub-sample/unsub-sample.component.ts
+++ src/app/unsub-sample/unsub-sample.component.ts
@@ -1,67 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, inject, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ReplaySubject } from 'dist/samples/chunk-OGFLKNU6';
-import { combineLatest, fromEvent, map, merge, Observable, startWith, switchMap, take } from 'rxjs';
-import { Replacer } from 'yaml/dist/doc/Document';
+import { BehaviorSubject, combineLatest, map, ReplaySubject, take } from 'rxjs';
 
 @Component({
   selector: 'se-unsub-sample',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Unsub sample</h1>
-    <button id="up">up</button>
-    <button id="down">down</button>
-    <p>id: {{id$ | async}}</p>
+    <h1>Unsubscribe sample</h1>
+    <p>Click the buttons to navigate to the next or previous id.</p>
+    <div *ngIf="vm$ | async as vm">
+      <button (click)="change$.next(1)">up</button>
+      <p>id: {{vm.id}}</p>
+      <button (click)="change$.next(-1)">down</button>
+    </div>
+    <style>
+      div {
+        display: flex;
+        flex-direction: row;
+        height: 3rem;
+        gap: .5rem;
+        justify-items: center;
+      }
+    </style>
   `,
-  styleUrls: ['./unsub-sample.component.css']
 })
 export class UnsubSampleComponent implements OnDestroy {
   route = inject(ActivatedRoute)
   router = inject(Router);
-  elm = inject(ElementRef).nativeElement as HTMLElement
 
-  init$ = new ReplaySubject(1)
+  id$ = new BehaviorSubject(120); // default ID, normally overwritten by the route.
+  change$ = new BehaviorSubject(0); // default change, normally overwritten by the buttons. (0 = no change
 
-  up$ = this.init$.pipe(
-    map(() => this.elm.querySelector('#up') as HTMLButtonElement),
-    switchMap((elm: HTMLButtonElement) => fromEvent(elm, 'click')),
-    map(() => 1),
-    startWith(0)
-  ) as Observable<number>
-
-  down$ = this.init$.pipe(
-    map(() => this.elm.querySelector('#down') as HTMLButtonElement),
-    switchMap((elm: HTMLButtonElement) => fromEvent(elm, 'click')),
-    map((x) => -1),
-    startWith(0)
-  ) as Observable<number>
-
-  initialId$ = this.route.params.pipe(
+  /** using a normal class field to holde the subscription */
+  sub = this.route.params.pipe(
+    /** read the initial value */
     map(params => parseInt(params.id, 10)),
     take(1), // Only take the first value in, as we don't want to cause an infinite loop.
   )
+    /**
+     * I'm subscribing here, because this is a sample on how to unsubscribe properly.
+     * Normally you would use the async pipe, and you don't need to subscribe.
+     * In this case, I could have just added the id to the vm$ observable!
+     */
+    .subscribe({
+      next: (id) => { this.id$.next(id) }
+    })
 
-  id$ = new ReplaySubject(1)
 
-  sub = combineLatest({
+  vm$ = combineLatest({
     id: this.id$,
-    change: merge(this.up$, this.down$)
-  }).subscribe(({ id, change }) => {
-    const goTo = id + change;
-    console.log({ id, change, goTo })
-    if (goTo > 0 && goTo !== id) {
-      this.router.navigate(['unsubSample', goTo])
-    }
-  })
-
-  ngOnInit() {
-    this.init$.next()
-  }
+    change: this.change$
+  }).pipe(
+    map(({ id, change }) => {
+      const goTo = id + change;
+      console.log({ id, change, goTo })
+      if (goTo > 0 && goTo !== id) {
+        this.router.navigate(['unsubSample', goTo])
+      }
+      return { id, goTo, change }
+    })
+  )
 
   ngOnDestroy() {
-    this.sub.unsubscribe()
+    this.sub.unsubscribe() // unsubscribe when the component is destroyed. 1 line of code...
   }
 
 }
