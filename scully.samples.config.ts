f2f6f5df790b57f3dfc55ca076800a00b42d1ca4
sanderelias <sanderelias+git@gmail.com>
2022-12-16 12:02:09 +0100

    fix missign routes in scully conf

--- scully.samples.config.ts
+++ scully.samples.config.ts
@@ -1,11 +1,14 @@
 import { enableSPS, ScullyConfig } from '@scullyio/scully';
 import '@scullyio/scully-plugin-puppeteer'
+import  routes from "./src/assets/routes.json" assert { type: "json" };
 
+const extraRoutes = routes.map(route => route.path)
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "samples",
   spsModulePath: "src/app/app.sps.module.ts",
+  extraRoutes,
   outDir: './dist/static',
   routes: {
   }
