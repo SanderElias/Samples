19f2ef0ec71affd08974a110f7c03a711f9246f9
sanderelias <sanderelias+git@gmail.com>
2023-02-23 10:27:40 +0100

    article updated

---
+++ quickTsExperiments/expand-ng-signals.ts
@@ -0,0 +1,33 @@
+import { computed, effect, Signal, signal } from './signals/index.js'
+import { Observable } from 'rxjs'
+
+
+const a = signal(1)
+const result = computed(() => a() * a());
+
+// log the result of the computation
+effect(() => console.log('a', a(), result()))
+
+/**
+ * sample of effect, simulating stuff that _will_ happen in apps if not prevented.
+ * I know this is so obvioous none is going to do it like this.
+ * But effects will call other functions/methods that will call other functions/methods, etc.
+ * at that point this might be way more hidden. And if there is no _asycn_ in the mix, the app will just freeze.
+ */
+effect(() => {
+  if (result() % 10 === 0) {
+    a.set(a() - 4)
+  } else {
+    /** using setTimeout to prevent 100% loop inside of 1 cycle */
+    setTimeout(() => a.set(a() + 2), 500)
+  }
+})
+
+a.set(3)
+
+
+
+
+
+
+
---
+++ quickTsExperiments/signals/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export {isSignal, Signal, ValueEqualityFn} from './src/api.js';
+export {computed} from './src/computed.js';
+export {setActiveConsumer} from './src/internal.js';
+export {SettableSignal, signal} from './src/signal.js';
+export {untrack} from './src/untrack.js';
+export {Watch} from './src/watch.js';
+export {effect} from './src/effect.js';
