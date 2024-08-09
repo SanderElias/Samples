b227b6b6f2092d76f214f9eaf79c89788b66aeb9
sanderelias <sanderelias+git@gmail.com>
2024-08-09 12:14:52 +0200

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,8 @@ import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   {
-    files: ['src/**/*.ts'],
-    ignores: ['src/**/*.spec.ts'],
+    files: ['./**/*.ts'],
+    ignores: ['./**/*.spec.ts'],
 
     plugins: {
       '@typescript-eslint': typescriptEslint,
@@ -29,17 +29,7 @@ export default [
     },
 
     rules: {
-      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
       'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
 
       '@angular-eslint/component-selector': [
         'error',
