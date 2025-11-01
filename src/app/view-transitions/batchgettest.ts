0d1d9b243a62f34649e2ffb8fad544553f845384
SanderElias <sanderelias+git@gmail.com>
2025-11-01 12:05:30 +0100

    feat: add viewtransitions playground

---
+++ src/app/view-transitions/batchgettest.ts
@@ -0,0 +1,25 @@
+const ids = [
+  {id: 'n8fgba1MaU8Hacmi'},
+  {id: 'jplzfyyB9b8ga7mO'},
+  {id: 'jd4G6I3Tan89aTmS'},
+  {id: '4Mcq6k9Jam8vaymZ'},
+  {id: 'stn47rvV9Y8UaTmi'},
+  {id: '3la6m6ya9L8Balmc'},
+  {id: 'vij7eJ01av8Ha6mt'},
+  {id: '72yb97vk998Nasmu'},
+  {id: 'hz4Oqi2aaK8Qa3m3'},
+  {id: '9Zuiwn9xaw8qavmb'},
+  {id: '6F16jFbYaZ8Va5m2'},
+  {id: 'clxZ1M89ad8ka1mY'},
+  {id: 'zzn08Szj9T8raBms'},
+  {id: 'kivCln5Tam85avma'},
+  {id: 'r57G8Z6Dah8ta8mm'},
+];
+
+import { signal } from '@angular/core';
+
+
+class batchGetter {
+  cue = new Map<string, Promise<any>>();
+
+}
