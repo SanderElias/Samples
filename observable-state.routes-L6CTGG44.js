a67c6ce76e871479fb8854fd1373b5002096f83f
SanderElias <SanderElias@users.noreply.github.com>
2026-01-15 15:37:48 +0000

    Deploying to gh-pages from @ SanderElias/Samples@5e46b227a38d980786862995a3321170c1c8849a 🚀

---
+++ observable-state.routes-L6CTGG44.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./observable-state.component-DF2EFFQB.js").then(o=>o.ObservableStateComponent)},{path:"hook",loadComponent:()=>import("./lifeycle-hooks-sample.component-LNGTAG2N.js").then(o=>o.LifeycleHooksSampleComponent)},{path:"**",redirectTo:""}];export{t as routes};
--- test.routes-T4JYBRPI.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var t=[{path:"testob",loadComponent:()=>import("./test.component-7JMOSXR2.js").then(e=>e.TestComponent)},{path:"**",redirectTo:"testob"}];export{t as routes};
