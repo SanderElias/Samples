fb0f95e52860fddee34c45b78e64d94a68938590
SanderElias <SanderElias@users.noreply.github.com>
2026-02-03 11:57:39 +0000

    Deploying to gh-pages from @ SanderElias/Samples@7db3997728570bcc1e55588dd2af97f8a43fbbe0 🚀

--- routes-4TMKLNGW.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var o=[{path:"",loadComponent:()=>import("./mqtt.component-5A3HJGCM.js").then(t=>t.MqttComponent)},{path:"list",loadComponent:()=>import("./device-list.component-FGJLN3ON.js").then(t=>t.DeviceListComponent)}],e=o;export{e as default,o as routes};
