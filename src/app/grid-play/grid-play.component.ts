94643cf53821bdd5238760cd45b747082ee82dd9
sanderelias <sanderelias+git@gmail.com>
2026-02-11 15:42:27 +0100

    chore: misc updates

--- src/app/grid-play/grid-play.component.ts
+++ src/app/grid-play/grid-play.component.ts
@@ -1,8 +1,12 @@
+import { httpResource } from '@angular/common/http';
 import {
   afterRenderEffect,
   Component,
   computed,
+  effect,
   ElementRef,
+  inject,
+  Injectable,
   signal,
   viewChild,
   type Signal
@@ -13,26 +17,22 @@ import { assertDefined } from '@se-ng/signal-utils';
   selector: 'se-grid-play',
   imports: [],
   template: `
-    <h1>Grid Play</h1>
-    <div
-      #gridContainer
-      class="grid-container"
-      (mousemove)="onMouseMove($event)"
-    >
+    <h1>Grid Play {{ user() }}</h1>
+    <div #gridContainer class="grid-container" (mousemove)="mouseToXY($event)">
       <div class="grid-item" [style.--x]="1" [style.--y]="1">1</div>
-      <div class="grid-item" [style.--x]="2" [style.--y]="2">2</div>
+      <div class="grid-item" [style.--x]="2" [style.--y]="2">3</div>
       <div
         class="mouse-indicator"
-        [style.--x]="mouseOver().x"
-        [style.--y]="mouseOver().y"
+        [style.--x]="mouseOnCell().x"
+        [style.--y]="mouseOnCell().y"
       ></div>
     </div>
-    <div id="cell-coords">{{ mouseOver().x }}, {{ mouseOver().y }}</div>
+    <div id="cell-coords">{{ mouseOnCell().x }}, {{ mouseOnCell().y }}</div>
   `,
   styleUrl: './grid-play.component.css'
 })
 export class GridPlayComponent {
-  mouseOver = signal(
+  mouseOnCell = signal(
     { x: 0, y: 0 },
     //  Custom equality function to prevent unnecessary updates when the mouse is in the same cell
     { equal: (a, b) => a.x === b.x && a.y === b.y }
@@ -49,28 +49,62 @@ export class GridPlayComponent {
     yCells: 0
   });
 
-  _ = afterRenderEffect(() => {
-    const gridContainer = this.gridContainer();
-    const style = getComputedStyle(gridContainer);
-    this.gridProps.set({
-      cellSize: parseFloat(style.getPropertyValue('--cellSize') || '10'),
-      gap: parseFloat(style.getPropertyValue('--gap') || '1'),
-      xCells: Number(style.getPropertyValue('--xCells') || '40'),
-      yCells: Number(style.getPropertyValue('--yCells') || '40')
-    });
+  _ = afterRenderEffect({
+    read: () => {
+      const gridContainer = this.gridContainer();
+      const style = getComputedStyle(gridContainer);
+      this.gridProps.set({
+        cellSize: parseFloat(style.getPropertyValue('--cellSize') || '10'),
+        gap: parseFloat(style.getPropertyValue('--gap') || '1'),
+        xCells: Number(style.getPropertyValue('--xCells') || '40'),
+        yCells: Number(style.getPropertyValue('--yCells') || '40')
+      });
+    }
   });
 
-  onMouseMove(event: MouseEvent) {
+  mouseToXY(event: MouseEvent) {
     const gridContainer = this.gridContainer();
     const { cellSize, gap, xCells, yCells } = this.gridProps();
     const rect = gridContainer.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
 
-    const cellSpan = cellSize + gap;
-    const x = Math.min(xCells, Math.floor(mouseX / cellSpan) + 1);
-    const y = Math.min(yCells, Math.floor(mouseY / cellSpan) + 1);
+    const sizeIncludingGap = cellSize + gap;
+    const x = Math.min(xCells, Math.floor(mouseX / sizeIncludingGap) + 1);
+    const y = Math.min(yCells, Math.floor(mouseY / sizeIncludingGap) + 1);
 
-    this.mouseOver.set({ x, y });
+    /** update the mouseOver signal with the new cell coordinates */
+    this.mouseOnCell.set({ x, y });
   }
+  user = inject(UserService).user;
+
+}
+
+/**
+ * @description
+ * @class UserService
+ */
+@Injectable({
+  providedIn: 'root'
+})
+class UserService {
+  userRs = httpResource<AutheliaStatus>(() => ({
+    url: 'https://auth.eliasweb.nl/api/state' ,
+    mode: 'cors',
+    credentials: 'include'
+  }));
+  user = computed(() =>
+    this.userRs.hasValue() ? this.userRs.value()?.data?.username : undefined
+  );
+}
+export interface AutheliaStatus {
+  status: string;
+  data:   Data;
+}
+
+export interface Data {
+  username:                string;
+  authentication_level:    number;
+  factor_knowledge:        boolean;
+  default_redirection_url: string;
 }
