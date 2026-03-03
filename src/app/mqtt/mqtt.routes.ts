7eb8d9f5b3ab01219f9e2f4d81959ddfa7ab6fce
sanderelias <sanderelias+git@gmail.com>
2026-03-03 11:23:19 +0100

    fix: mqttRoutes

--- src/app/mqtt/mqtt.routes.ts
+++ src/app/mqtt/mqtt.routes.ts
@@ -5,11 +5,7 @@ export const routes: Routes = [
     path: 'list',
     loadComponent: () =>
       import('./device-list.component').then(m => m.DeviceListComponent)
-  }
-];
-import type { Routes } from '@angular/router';
-
-export const routes: Routes = [
+  },
   {
     path: '',
     loadComponent: () => import('./mqtt.component').then(m => m.MqttComponent)
